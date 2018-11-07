704ea23af3bcc69ed6066fa09db98ad6fd95d3db
Alec <afroenygam@gmail.com>
2018-11-07 01:24:11 -0500

    spot object created

---
+++ spot.js
@@ -0,0 +1,66 @@
+function spot(i,j) {
+    this.i = i
+    this.j = j // Do these really need to be defined with "this"?
+
+    // Distances, heuristic(g), cost(h) and total (f)
+    this.f = 0
+    this.g = 0
+    this.h = 0
+
+    // Keep track of neighbors
+    this. neighbors = []
+
+    // Will be defined if there was a spot before it. 
+    this.previous = undefined
+
+    this.isWall = false
+    if (random(1) < 0.4)
+    {
+	this.isWall = true
+    }
+
+
+    this.show = function(col) {
+	if (isWall)
+	{
+	    fill(0)
+	    noStroke()
+	    ellipse(this.i * w + w/2, this.j * h + h/2, w/2, h/2)
+	}
+	else if (col) {
+	    fill(col)
+	    rect(this.i * w, this.j * h, w, h)
+	}
+	
+    }
+
+    this.addNeighbors = function(grid) {
+	var i = this.i
+	var j = this.j
+
+	if (i > 0) {
+	    this.neighbors.push(grid[i-1][j])
+	}
+	if (i < cols - 1) {
+	    this.neighbors.push(grid[i+][j]
+	}
+	if (j > 0) {
+	    this.neighbors.push(grid[i][j-1])
+	}
+	if (j < rows - 1) {
+	    this.neighbors.push(grid[i][j+1])
+	}
+	if (j > 0 && i > 0) {
+	    this.neighbors.push(grid[i-1][j-1])
+	}
+	if (i > 0 && j < rows -1) {
+	    this.neighbors.push(grid[i-1][j+1])
+	}
+	if (i < cols -1 && j > 0) {
+	    this.neighbors.push(grid[i+1][j-1])
+	}
+	if (i < cols - 1 && j < rows -1) {
+	    this.neighbors.push(grid[i+1][j+1])
+	}
+    }
+}
